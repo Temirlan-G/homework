411f1393a02610bc5b393e79af83b5ffb1729bd5
Temirlan <temirlan.tumanbekov@yandex.com>
2021-05-09 00:17:26 +0600

    v 1.2

--- HW2.py
+++ HW2.py
@@ -1,14 +1,43 @@
-from HW1 import Fraction
+class Complex:
 
-fr = Fraction(1, 2)
-fr2 = Fraction(2, 3)
-fr3 = Fraction(3, 4)
-f_result = fr + fr2 + fr3
-f_result2 = f_result - fr2 - fr
-f_result3 = fr * fr2 * fr3
-f_result4 = f_result3 / fr3 / fr2
-print(f_result)
-print(f_result2)
-print(f_result3)
-print(f_result4)
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def __add__(self, mnim):
+        z1 = self.a + mnim.a
+        z2 = self.b + mnim.b
+        return print(z1,'+', z2,'i')
+
+    def __sub__(self, mnim):
+        z1 = self.a - mnim.a
+        z2 = self.b - mnim.b
+        return print(z1,'+', z2,'i')
+
+    def __mul__(self, mnim):
+        z1 = self.a * mnim.a - self.b * mnim.b
+        z2 = self.a * mnim.b + mnim.a * self.b
+        return print(z1, '+', z2, 'i')
+
+    def __truediv__(self, mnim):
+        z1 = (self.a * self.b + mnim.a * mnim.b) / (self.b**2 + mnim.b**2)
+        z2 = (mnim.a * self.b - self.a * mnim.b) / (self.b**2 + mnim.b**2)
+        return print(z1, '+', z2, 'i')
+
+# self.a = a
+# mnim.a = b
+# self.b = c
+# mnim.b = d
+
+cmpls = Complex(5, 3)
+cmpls1 = Complex(8, 2)
+
+cmplx = Complex(-5, -1)
+cmplx1 = Complex(-1, 1)
+
+add = cmpls + cmpls1
+sub = cmpls - cmpls1
+mul = cmpls * cmpls1
+div = cmplx / cmplx1
+
 
